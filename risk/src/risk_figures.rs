867b9a8a38023b15b1911f3459b42a895c8e433c
snusnusnu <35423703+snusnusnu@users.noreply.github.com>
2022-03-01 14:53:06 +0100

    code improvement

--- risk/src/risk_figures.rs
+++ risk/src/risk_figures.rs
@@ -25,22 +25,25 @@ use crate::error::RiskError;
 //         // further checks
 //     }
 // }
+use std::ops::{Sub, Div};
 
 pub trait SharpeRatio {
-    type N: std::ops::Sub<Output = Self::N> + std::ops::Div<Output = Self::N> + Clone;
+    type N: Sub<Output = Self::N> + Div<Output = Self::N> + Clone;
 
-    fn not_divisible(n: Self::N, tolerance: Option<Self::N>) -> bool;
+    fn not_divisible(n: Self::N, threshold: Option<Self::N>) -> bool;
 
-    /// The ratio of the expected value of the excess of the asset return and the benchmark return,
-    /// over the risk of the asset.
-    /// https://en.wikipedia.org/wiki/Sharpe_ratio
+    /// The ratio of the expected value of the excess of the asset returns and the risk-free (or benchmark) returns,
+    /// over the 'risk' (standard deviation) of the excess of asset and the risk-free (or benchmark) returns.
+    /// Use the threshold for the division by 'risk'.
+    /// See https://en.wikipedia.org/wiki/Sharpe_ratio
+    /// resp. https://en.wikipedia.org/wiki/Information_ratio
     fn calculate(
         asset_return: Self::N,
         benchmark_return: Self::N,
         asset_std: Self::N,
-        tolerance: Option<Self::N>,
+        threshold: Option<Self::N>,
     ) -> Result<Self::N, RiskError> {
-        if Self::not_divisible(asset_std.clone(), tolerance) {
+        if Self::not_divisible(asset_std.clone(), threshold) {
             return Err(RiskError::ZeroDivision);
         }
         let sr = (asset_return - benchmark_return) / asset_std;
@@ -50,9 +53,9 @@ pub trait SharpeRatio {
 
 
 #[macro_export]
-macro_rules! impl_sharpe_ratio {
-    ($impl_type:ty) => {
-        impl SharpeRatio for $impl_type {
+macro_rules! impl_risk_figure {
+    ($rf:ident<$impl_type:ty>) => {
+        impl $rf for $impl_type {
             type N = $impl_type;
             fn not_divisible(f: Self::N, tolerance: Option<Self::N>) -> bool {
                 match tolerance {
@@ -64,10 +67,21 @@ macro_rules! impl_sharpe_ratio {
     };
 }
 
-impl_sharpe_ratio! { f32 }
-impl_sharpe_ratio! { f64 }
+impl_risk_figure! { SharpeRatio<f32> }
+impl_risk_figure! { SharpeRatio<f64> }
 
 // TODO:
 // use bigint with feature
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn sharpe_ratio_f32() {
+    //     let a = 0.2_f32;
+    //     let res : Result<f32, RiskError> = SharpeRatio::calculate(a, 0.1_f32, 1.0_f32, None);
+    //     assert_eq!(SharpeRatio::calculate(0.2_f32, 0.1_f32, 1.0_f32, None).unwrap(), 0.1_f32);
+    // }
+}
